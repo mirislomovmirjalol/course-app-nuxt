cc100005dfe3a46a39133431a5c086eae84b097b
mirjalol <mirislomovmirjalol@gmail.com>
2023-04-21 22:33:27 +0100

    code formatting

--- pages/course.vue
+++ pages/course.vue
@@ -15,9 +15,9 @@ const {chapters} = useCourse();
       </h1>
     </div>
 
-    <div class="flex flex-row justify-center flex-grow">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div
-          class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+          class="prose p-8 bg-white rounded-md flex flex-col border border-gray-300"
       >
         <h3 class="text-lg font-bold">Chapters</h3>
         <div
@@ -25,24 +25,36 @@ const {chapters} = useCourse();
             v-for="chapter in chapters"
             :key="chapter.slug"
         >
-          <h4>{{ chapter.title }}</h4>
+          <h4 class="font-bold mt-4">{{ chapter.title }}</h4>
           <NuxtLink
               v-for="(lesson, index) in chapter.lessons"
               :key="lesson.slug"
-              class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4"
-              :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+              class="flex flex-row space-x-1 no-underline prose-sm font-normal py-1 px-4 -mx-4 rounded-md hover:bg-gray-200"
+              :to="lesson.path"
           >
-            <span class="text-gray-500"
-            >{{ index + 1 }}.</span
+            <span
+                class="text-gray-500"
             >
-            <span>{{ lesson.title }}</span>
+              {{ index + 1 }}
+            </span>
+            <span>
+              {{ lesson.title }}
+            </span>
           </NuxtLink>
         </div>
       </div>
 
-      <div class="prose p-12 bg-white rounded-md w-[65ch]">
+      <div
+          class="prose p-12 bg-white rounded-md w-full border border-gray-300"
+      >
         <NuxtPage/>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.router-link-active {
+  @apply bg-gray-200;
+}
+</style>
