7b75e4a46289d0c55668c1ffbea8ace6edb19c71
mirjalol <mirislomovmirjalol@gmail.com>
2023-04-21 22:35:06 +0100

    lessons

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,5 +1,14 @@
 import courseData from "~/composables/courseData";
 
 export const useCourse = () => {
-    return courseData;
+    return {
+        ...courseData,
+        chapters: courseData.chapters.map((chapter) => ({
+            ...chapter,
+            lessons: chapter.lessons.map((lesson) => ({
+                ...lesson,
+                path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+            })),
+        })),
+    };
 }
